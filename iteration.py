6e8a4cecc8afb7be4ff5a887e92dc7c0eb3de66d
Abram Hindle <abram.hindle@softwareprocess.es>
2013-09-02 23:43:23 -0600

    python talk

---
+++ iteration.py
@@ -0,0 +1,18 @@
+# while loop
+
+condition = True
+while condition:
+    """block"""
+    condition = not condition
+# condition is False
+
+x = 10
+while x > 0:
+    x = x - 1
+# x is 0
+
+# maybe you're not sure how many
+# iterations you need?
+x = 100.0
+while x > 1:
+    x = x / 3
