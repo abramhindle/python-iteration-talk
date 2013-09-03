d97be5fd416c87706628ab8ed4adab11244d77cd
Abram Hindle <abram.hindle@softwareprocess.es>
2013-09-03 00:03:38 -0600

    for loops

--- iteration.py
+++ iteration.py
@@ -16,3 +16,17 @@ while x > 0:
 x = 100.0
 while x > 1:
     x = x / 3
+
+# for loop
+sum = 0
+for i in range(1,10):
+    sum = sum + i
+# sum is 45
+sum = 0
+for i in xrange(1,100000):
+    sum = sum + i
+# sum is 4 999 950 000
+s = ""
+for elm in ["a","b","c"]:
+    s = s + elm
+# s is abc
