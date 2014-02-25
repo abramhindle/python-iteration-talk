4dbb3d5f52883cd2278a9e9ba8dae4e7fc90a221
Abram Hindle <abram.hindle@ualberta.ca>
2014-02-25 11:33:31 -0700

    licensing

--- src/softwareprocess.es/abramhindle/iteration/iteration.go
+++ src/softwareprocess.es/abramhindle/iteration/iteration.go
@@ -1,5 +1,23 @@
 package main
 
+/*
+Copyright 2014 Abram Hindle
+Copyright 2013 Ewen Cheslack-Postava iterator inspiration http://ewencp.org/blog/golang-iterators/
+Copyright 2012 Stephen Weinberg (reflection example with slices from https://stackoverflow.com/questions/12753805/type-converting-slices-of-interfaces-in-go/12754757#12754757)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import "fmt"
 import "os"
 import "bufio"
